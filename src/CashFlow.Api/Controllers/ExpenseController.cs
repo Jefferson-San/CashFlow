50e4e6011891553a08344701f07eca0182e4bd01
Jefferson Santos <jeffsan.dev@gmail.com>
2025-12-01 21:38:38 -0300

    feature: Criação do ExpenseController

---
+++ src/CashFlow.Api/Controllers/ExpenseController.cs
@@ -0,0 +1,30 @@
+﻿using CashFlow.Application.Commands.CreateExpense;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpenseController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ExpenseController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateExpense([FromBody] CreateExpenseCommand request)
+        {
+            var result = await _mediator.Send(request);
+            if (!result.IsSuccess)
+                return BadRequest(result.Error);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ src/CashFlow.Api/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using CashFlow.Application.Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+using System.Text.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using Error = CashFlow.Application.Common.Error;
+
+namespace CashFlow.Api.Filters;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next; 
+        _logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Erro não tratado. TraceId={TraceId}", context.TraceIdentifier);
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            Error error = Error.Failure(
+                message: "Ocorreu um erro desconhecido",
+                notifications: null
+                );
+
+            var response = ResultViewModel<object>.Failure(error);
+
+
+            var json = JsonSerializer.Serialize(response);
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
