b40207716ca49c7735f303e317ae20ba60af0c4b
Jefferson Santos <jeffsan.dev@gmail.com>
2025-12-05 21:24:38 -0300

    feature: adiciona DI de applications

--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -1,14 +1,18 @@
 using CashFlow.Api.Filters;
+using CashFlow.Application;
 using CashFlow.Infrastructure;
 using MediatR;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.Load("CashFlow.Application")));
 
 var app = builder.Build();
