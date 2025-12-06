b61a70e7a370e90be324057931c7ce0375785f04
Jefferson Santos <jeffsan.dev@gmail.com>
2025-12-05 21:21:20 -0300

    feature: configura autoMapper

---
+++ src/CashFlow.Application/Mapper/AutoMapperConfig.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using CashFlow.Application.Commands.CreateExpense;
+using CashFlow.Domain.Entities;
+
+namespace CashFlow.Application.Mapper;
+public class AutoMapperConfig : Profile
+{
+    public AutoMapperConfig()
+    {
+        MapToEntity();
+        MapToViewModel();
+    }
+
+    private void MapToEntity()
+    {
+        CreateMap<CreateExpenseCommand, Expense>();
+    }
+
+    private void MapToViewModel()
+    {
+
+    }
+}
