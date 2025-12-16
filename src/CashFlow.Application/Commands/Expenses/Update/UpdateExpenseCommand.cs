92e9a558b49ff4146ecc738fc947d197dee2fe4c
Jefferson Santos <jeffsan.dev@gmail.com>
2025-12-16 09:02:55 -0300

    feature: criação do endpoint para atualização de uma despesa

---
+++ src/CashFlow.Application/Commands/Expenses/Update/UpdateExpenseCommand.cs
@@ -0,0 +1,33 @@
+﻿using CashFlow.Application.Commands.Expenses.Create;
+using CashFlow.Application.Common;
+using CashFlow.Domain.Enums;
+using Flunt.Notifications;
+using Flunt.Validations;
+using MediatR;
+
+namespace CashFlow.Application.Commands.Expenses.Update;
+public class UpdateExpenseCommand : Notifiable<Notification>, IRequest<ResultViewModel<Guid>>
+{
+    public Guid Id { get; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public int Amount { get; set; }
+    public PaymentType PaymentType { get; set; }
+
+    public void Validate()
+    {
+        AddNotifications(
+            new Contract<CreateExpenseCommand>()
+            .Requires()
+            .IsNotEmpty(Id, "Id")
+            .IsNotNullOrEmpty(Title, "Title")
+            .IsGreaterThan(0, Amount, "Amount")
+            .IsTrue(Enum.IsDefined(typeof(PaymentType), PaymentType), "PaymentType", "Invalid value")
+            );
+    }
+
+    public void AddNotificationNotFound()
+    {
+        AddNotification("Error", "Expense not found");
+    }
+}
