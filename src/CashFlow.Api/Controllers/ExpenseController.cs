bbf41f3ddabf6551cb7f81baa8ee952b2fdf058e
Jefferson Santos <jeffsan.dev@gmail.com>
2025-12-17 08:18:11 -0300

    fix: corrige using de DetailsExpenseQuery

--- src/CashFlow.Api/Controllers/ExpenseController.cs
+++ src/CashFlow.Api/Controllers/ExpenseController.cs
@@ -1,6 +1,5 @@
 ﻿using CashFlow.Application.Commands.Expenses.Create;
 using CashFlow.Application.Commands.Expenses.Update;
-using CashFlow.Application.Queries.DetailsExpense;
 using CashFlow.Application.Queries.Expenses.DetailsExpense;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
