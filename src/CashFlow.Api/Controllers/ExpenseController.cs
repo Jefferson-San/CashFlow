1ba8bd4383227e97958140b17bf9a302289e2472
Jefferson Santos <jeffsan.dev@gmail.com>
2025-12-17 08:18:53 -0300

    fix: formatação

--- src/CashFlow.Api/Controllers/ExpenseController.cs
+++ src/CashFlow.Api/Controllers/ExpenseController.cs
@@ -4,47 +4,46 @@ using CashFlow.Application.Queries.Expenses.DetailsExpense;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CashFlow.Api.Controllers
+namespace CashFlow.Api.Controllers;
+
+[Route("v1/api/[controller]")]
+[ApiController]
+public class ExpenseController : ControllerBase
 {
-    [Route("v1/api/[controller]")]
-    [ApiController]
-    public class ExpenseController : ControllerBase
+    private readonly IMediator _mediator;
+
+    public ExpenseController(IMediator mediator)
     {
-        private readonly IMediator _mediator;
+        _mediator = mediator;
+    }
 
-        public ExpenseController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
+    [HttpPost]
+    public async Task<IActionResult> CreateExpense([FromBody] CreateExpenseCommand request)
+    {
+        var result = await _mediator.Send(request);
+        if (!result.IsSuccess)
+            return BadRequest(result.Error);
 
-        [HttpPost]
-        public async Task<IActionResult> CreateExpense([FromBody] CreateExpenseCommand request)
-        {
-            var result = await _mediator.Send(request);
-            if (!result.IsSuccess)
-                return BadRequest(result.Error);
+        return Ok(result);
+    }
 
-            return Ok(result);
-        }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetExpenseById(Guid id)
+    {
+        var result = await _mediator.Send(new DetailsExpenseQuery(id));
+        if(!result.IsSuccess)
+            return BadRequest(result.Error);
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetExpenseById(Guid id)
-        {
-            var result = await _mediator.Send(new DetailsExpenseQuery(id));
-            if(!result.IsSuccess)
-                return BadRequest(result.Error);
+        return Ok(result);
+    }
 
-            return Ok(result);
-        }
+    [HttpPut]
+    public async Task<IActionResult> UpdateExpense([FromBody] UpdateExpenseCommand request)
+    {
+        var result = await _mediator.Send(request);
+        if(!result.IsSuccess)
+            return BadRequest(result.Error);
 
-        [HttpPut]
-        public async Task<IActionResult> UpdateExpense([FromBody] UpdateExpenseCommand request)
-        {
-            var result = await _mediator.Send(request);
-            if(!result.IsSuccess)
-                return BadRequest(result.Error);
-
-            return Ok(result);
-        }
+        return Ok(result);
     }
 }
